e655fd1d280a7945078f6760fd639678e96415d6
Lucas Fernandes <lucasfernandespontes122@gmail.com>
2025-04-02 19:12:29 -0300

    Commit Inicial

---
+++ array.js
@@ -0,0 +1,13 @@
+const paises = ['Brasil', 'Canada', 'China'] // Uma Lista de Dados
+
+console.log('Acessando os Pais: ' + paises[2]) // Acessando alguma posição do array 
+console.log('Tamanho do array: ', paises.length) // Tamanho do Array
+
+// Operação com array 
+
+paises.push('USA') //Adicionar no final do Array 
+paises.pop() //Remove o ultimo elemento
+paises.unshift('França')
+paises.shift()
+
+console.log(paises)
---
+++ vari.js
@@ -0,0 +1,30 @@
+//Dicas de Variaveis
+var nome = 'Eduardo' // string - Texto 
+nome = 'Pedro'
+
+console.log(nome)
+
+//Ele pode ser Alterado 
+
+let idade = 20 //numeros
+    idade = 15
+
+console.log(idade)
+
+//NÃO pode ser alterado
+const cidade = 'João Pessoa'
+
+var maior_idade = false // bolean Verdadeiro ou Falso
+
+const paises = ['Brasil', 'Canada', 'China'] // Uma Lista de Dados
+
+//Objeto
+let devices = {
+    nome: 'iphone 14',
+    preco: 8000,
+    cores: {
+        cor1: 'Vermelho',
+        cor2: 'azul'
+    }
+}
+
