9b2c8728480853ab77670fab04e3090e96127d08
Lucas Fernandes <lucasfernandespontes122@gmail.com>
2025-04-09 20:43:30 -0300

    Commit 2

---
+++ exe_funcao.js
@@ -0,0 +1,17 @@
+//Criar uma função 
+//Que Soma 2 numeros e retorna esses numeros somado
+//Deve ser enviado para parametros os 2 numeros da conta
+
+//Numero1: 20 
+//Numero2: 10 
+//Soma: 30
+
+function soma(numero1, numero2) {
+    const resultado = numero1 + numero2
+    console.log('Numero1: ', numero1)
+    console,log('Numero2: ', numero2)
+    console.log('Soma', resultado)
+
+}
+
+soma(12,8)
---
+++ funcoes.js
@@ -0,0 +1,25 @@
+// Precisa Chamar 
+// Pode ou não receber Parametros
+// Pode ou não retornar Dados
+
+//function gerarNome() {
+    //const nome = 'Lucas'
+    //const sobrenome = ' Fernandes'
+    //const idade = '24'
+    
+    //console.log('Nome: ', nome + sobrenome)
+  //  console.log('Idade: ', idade)
+//}
+
+//gerarNome()
+
+
+
+// Recebendo Parametros 
+function gerarNome(nome, sobrenome, idade) {
+    console.log('Nome: ', nome + sobrenome)
+    console.log('Idade: ', idade)
+}
+
+gerarNome('Tadeu', 'Luiz', 44)
+gerarNome('Laura', 'Estafany', 23)
